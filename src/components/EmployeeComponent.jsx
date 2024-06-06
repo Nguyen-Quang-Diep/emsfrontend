69b79b2da4f44b993763ab594ffc2bd11d9b8e3b
DiepNguyen <nguyenquangdiepnx1@gmail.com>
2024-06-06 10:45:19 +0700

    Add Employee validate from in react app

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react"
+import { createEmployee } from "../services/EmployeeService"
+import { useNavigate } from "react-router-dom"
 
 export const EmployeeComponent = () => {
     const [firstName,setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+
+    const [error,setError] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+
+    const navigator = useNavigate()
 
     const handleFirstName = (e) => {
         setFirstName(e.target.value)
@@ -15,10 +25,51 @@ export const EmployeeComponent = () => {
         setEmail(e.target.value)
     }
     const saveEmployee = (e) => {
+
         e.preventDefault();
-        const employee = {firstName , lastName, email}
-        console.log(employee)
+
+        if(validateForm()){
+            const employee = {firstName , lastName, email}
+            console.log(employee)
+            createEmployee(employee).then((response) =>{
+                console.log(response.data);
+                navigator('/employees')
+            })
+        }
+        
     }
+
+    function validateForm(){
+        let valid = true
+
+        const errorCopy = {... error}
+
+        if(firstName.trim()){
+            errorCopy.firstName = ''
+        }else{
+            errorCopy.firstName = 'First Name is Required'
+            valid = false
+        }
+
+        if(lastName.trim()){
+            errorCopy.lastName = ''
+        }else{
+            errorCopy.lastName = 'Last Name is Required'
+            valid = false
+        }
+
+        if(email.trim()){
+            errorCopy.email = ''
+        }else {
+            errorCopy.email = 'email is required'
+            valid = false
+        }
+
+        setError(errorCopy)
+
+        return valid
+    }
+
   return (
     <div className="container">
         <br />
@@ -32,9 +83,10 @@ export const EmployeeComponent = () => {
                             <input type="text" placeholder="Enter Employee First Name"
                                 name="firstName"
                                 value={firstName}
-                                className="form-control"
+                                className={`form-control ${error.firstName ? 'is-invalid' : ''}`}
                                 onChange={handleFirstName}
                                 />
+                            {error.firstName && <div className="invalid-feedback">{error.firstName}</div>}
                         </div>
 
                         <div className="form-group mb-2">
@@ -42,9 +94,10 @@ export const EmployeeComponent = () => {
                             <input type="text" placeholder="Enter Employee Last Name"
                                 name="lastName"
                                 value={lastName}
-                                className="form-control"
+                                className={`form-control ${error.lastName ? 'is-invalid' : ''}`}
                                 onChange={handleLastName}
                                 />
+                            {error.lastName && <div className="invalid-feedback">{error.lastName}</div>}
                         </div>
 
                         <div className="form-group mb-2">
@@ -52,9 +105,10 @@ export const EmployeeComponent = () => {
                             <input type="email" placeholder="Enter Employee's Email"
                                 name="email"
                                 value={email}
-                                className="form-control"
+                                className={`form-control ${error.email ? 'is-invalid' : ''}`}
                                 onChange={handleEmail}
                                 />
+                            {error.email && <div className="invalid-feedback">{error.email}</div>}
                         </div>
 
                         <button className="btn btn-success" onClick={saveEmployee}>Submit</button>
